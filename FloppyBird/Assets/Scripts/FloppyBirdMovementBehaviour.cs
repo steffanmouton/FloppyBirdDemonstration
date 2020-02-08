669145b36b422f79a7ce77b3b6b9cbde5af00edb
Steffan Mouton <smouton8993@gmail.com>
2020-02-07 20:14:48 -0600

    Created FloppyBird. (Didn't stage all changes last time).

---
+++ FloppyBird/Assets/Scripts/FloppyBirdMovementBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class FloppyBirdMovementBehaviour : MonoBehaviour
+{
+    public float flapForce;
+    private Rigidbody rb;
+    private Transform childT;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        childT = GetComponentInChildren<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * flapForce, ForceMode.VelocityChange);
+            childT.localScale = new Vector3(.75f,1.25f,1f);
+        }
+        childT.localScale = Vector3.Lerp(transform.localScale, Vector3.one, Time.deltaTime * 5f);
+    }
+}
---
+++ FloppyBird/Assets/Scripts/ObstacleSetBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleSetBehaviour : MonoBehaviour
+{
+    public float moveSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.forward * (moveSpeed * Time.deltaTime);
+    }
+}
